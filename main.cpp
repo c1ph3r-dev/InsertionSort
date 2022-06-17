f43828c7d1a124358cafda34ac682ec3a483089b
Jonathan Rampersad <c1ph3r.dev@gmail.com>
2022-06-17 08:09:16 -0400

    Added comments

--- main.cpp
+++ main.cpp
@@ -4,20 +4,20 @@ void InsertionSort(int* array, int size)
 {
     for (int i = 1; i < size; i++)
     {
-        int current = array[i];
+        int current = array[i]; // Stores current value in array
         int j = i - 1;
-        while (j >= 0 && array[j] >= current)
+        while (j >= 0 && array[j] >= current) // While previous values are greater than current value
         {
-            array[j + 1] = array[j];
+            array[j + 1] = array[j]; // Copy larger values to further in array
             j--;
         }
-        array[j + 1] = current;
+        array[j + 1] = current; // Place current value in correct position in array
     }
 }
 
 int main()
 {
-    int test[] = {1, 2, 8, 4, 7, 3};
+    int test[] = {9, 5, 8, 4, 7, 3};
     InsertionSort(test, sizeof(test)/sizeof(test[0]));
 
     for (auto i: test)
